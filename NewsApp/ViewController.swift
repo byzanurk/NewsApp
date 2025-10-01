d6872b56612ca31126043f5395597bd27c6b83c9
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-10-01 17:21:58 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Beyza Nur Tekerek on 1.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
