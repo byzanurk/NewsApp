2795e5acb556c6361eb380348c8c22745722d399
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-10-03 02:16:08 +0300

    update new collection view cell and xib file

--- NewsApp/Views/Cells/NewCollectionViewCell.swift
+++ NewsApp/Views/Cells/NewCollectionViewCell.swift
@@ -19,11 +19,17 @@ class NewCollectionViewCell: UICollectionViewCell {
         super.awakeFromNib()
         contentView.layer.cornerRadius = 8
         contentView.layer.masksToBounds = true
+        
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.15
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        layer.shadowRadius = 8
+        layer.masksToBounds = false
     }
 
     func configure(with article: Article) {
         titleLabel.text = article.title
-        authorLabel.text = article.author
+        authorLabel.text = article.author ?? article.source?.name
         dateLabel.text = article.publishedAt
         
         if let urlString = article.urlToImage, let url = URL(string: urlString) {
