5d4cfe2f7a769b106ffcf7d016ef1e9f4d51380d
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-10-02 22:20:19 +0300

    add basevc

---
+++ NewsApp/Screens/BaseView/BaseViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BaseViewController.swift
+//  NewsApp
+//
+//  Created by Beyza Nur Tekerek on 2.10.2025.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    var pageTitle: String = "" {
+        didSet {
+            self.title = pageTitle
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.titleTextAttributes = [
+            .foregroundColor: UIColor.label,
+            .font: UIFont.systemFont(ofSize: 17, weight: .semibold)
+        ]
+    }
+}
