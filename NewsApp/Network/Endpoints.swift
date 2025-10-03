d99305870edfbce203cd98cc1010675b35570408
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-10-03 19:02:00 +0300

    add new endpoint

--- NewsApp/Network/Endpoints.swift
+++ NewsApp/Network/Endpoints.swift
@@ -9,11 +9,14 @@ import Foundation
 
 enum NewsEndpoint {
     case topHeadlines (category: String, country: String = "us")
+    case everything (query: String)
     
     var path: String {
         switch self {
         case .topHeadlines:
             return "/v2/top-headlines"
+        case .everything:
+            return "/v2/everything"
         }
     }
     
@@ -28,6 +31,13 @@ enum NewsEndpoint {
                 "country" : country,
                 "category" : category,
                 "apiKey" : "d0c5a805f78549f293ff8b2aa970139a"
+            ]
+        case .everything(let query):
+            return [
+                "apiKey": "d0c5a805f78549f293ff8b2aa970139a",
+                "q": query,
+                "sortBy": "publishedAt",
+                "language": "en"
             ]
         }
     }
